faefe331e81ae039c3eb996ce629b23d41f6b854
Toby Thomas <Toby@TobysMBTouchbar.modem>
2022-05-25 21:22:05 +1000

    25/05/22

--- public/todolist.js
+++ public/todolist.js
@@ -1,67 +1,155 @@
-// Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const button = document.querySelector("#taskform > button"); // Complex CSS query
-const tasklist = document.getElementById("tasklist");
-const taskInput = document.getElementById("taskInput");
+////////////////////////////////////////
+// This code runs in global scope. It gets executed when the <script> tag in the HTML is loaded.
+////////////////////////////////////////
 
-// Event listener for Button click
-// This could also be form.addEventListener("submit", function() {...} )
-button.addEventListener("click", function(event) {
-  event.preventDefault(); // Not as necessary for button, but needed for form submit
+// Bind an event to the submit button to capture information from the form and store it into localStorage.
+let subButton = document.getElementById("itemsubmit");
 
-  let task = form.elements.task.value; // could be swapped out for line below
-  //let task = taskInput.value;
+// Render the items from local storage so the page appears correct when it loads.
+renderItems();
 
-  let date = (new Date()).toLocaleDateString('en-UK') //Convert to short date format
+// The event listener gets bound to the submit button and the contents of the internal function are run when the button is clicked.
+subButton.addEventListener("click", function() {
 
-  // Call the addTask() function using
-  addTask(task, date, "26/03/2021", "Low", ["1", "30"], false);
+  // Start by getting the form values.
+  let taskName = document.getElementById("taskname").value;
+  let dueDate = document.getElementById("duedate").value;
+  let timeInput = document.getElementById("timeinput").value;
+  let importanceInput = document.getElementById("importanceinput").value;
 
-  // Log out the newly populated taskList everytime the button has been pressed
-  console.log(taskList);
-})
-
-// Create an empty array to store our tasks
-var taskList = [];
-
-function addTask(taskDescription, createdDate, dueDate, importance, urgency, estimatedTime, completionStatus) {
-  let task = {
-    taskDescription,
-    createdDate,
-    dueDate,
-    importance,
-    urgency,
-    estimatedTime,
-    completionStatus
+  // Make a JS object to contain the data we want to write into local storage for each item. This is nice because we can have one key:value pair as we do here, or 50.
+  let itemObj = {
+    'taskName': taskName,
+    'dueDate': dueDate,
+    'timeInput': timeInput,
+    'importanceInput': importanceInput
   };
 
-  // Add the task to our array of tasks
-  taskList.push(task);
 
-  // Separate the DOM manipulation from the object creation logic
-  renderTask(task);
+  // Get the item list from localStorage. This uses a custom function, since we need to do this action in a few different places. See that function for deets of how it works.
+  let existingItems = getItems();
+
+  // Add the new item onto the end of the list.
+  existingItems.push(itemObj);
+
+
+  // Local storage can only store strings, while we want to store an array. To get around this, we use JSON.stringify (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify).
+  existingItems = JSON.stringify(existingItems);
+
+  // And finally we write the JSON string into local storage.
+  localStorage.setItem('items', existingItems);
+
+  // Render the items using our custom function.
+  renderItems();
+});
+
+////////////////////////////////////////
+// Now we have some custom functions to handle tasks we have to do in order to make all this work.
+////////////////////////////////////////
+
+// getItems should be fairly self explanatory. It gets items from local storage!
+function getItems() {
+  // Check to see if we have any item items in local storage already
+  let items = localStorage.getItem('items');
+
+  // If the value of the items variable is `null` then we have not created or used localStorage yet. If this is true, we create an empty array and return that to the code that ran getItems(). 
+  if (items == null) {
+    return [];
+  }
+
+  // If we are still here, then we do have items in the list, or the list is empty.
+  // Either way, we convert that information back into an array using JSON.parse (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse), this is the opposite action to JSON.stringify.
+  items = JSON.parse(items);
+
+  // Return the list of items back to the code that ran this function.
+  return items;
 }
 
+// Render the items to the screen using the DOM manipulation methods.
+function renderItems() {
+  // Use our custom getItems() function to retrieve info from local storage.
+  let items = getItems();
 
-// Function to display the item on the page
-function renderTask(task) {
-  let item = document.createElement("li");
-  item.innerHTML = "<p>" + task.taskDescription + "</p>";
+  // Find the UL element within the #itemlist DIV.
+  let itemUl = document.querySelector('#itemlist ul');
 
-  tasklist.appendChild(item);
+  // Clear the contents of the UL to rebuild it fresh.
+  itemUl.innerHTML = ""; // <-- this is the one time I'm okay with you using innerHTML. Otherwise build the DOM elements properly and don't concatenate strings :)
 
-  // Setup delete button DOM elements
-  let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete");
-  delButton.appendChild(delButtonText);
-  item.appendChild(delButton); // Adds a delete button to every task
+  // forEach is like a shorthand for() loop. It runs the internal function once per item in the array.
+  items.forEach(function(item) {
 
-  // Listen for when the 
-  delButton.addEventListener("click", function(event){
-    item.remove(); // Remove the task item from the page when button clicked
-    // Because we used 'let' to define the item, this will always delete the right element
-  })
-  
-  // Clear the value of the input once the task has been added to the page
-  form.reset();
+    // Create a li DOM element to hold each item
+    let itemLi = document.createElement('li');
+
+    // Now we could just set innerText or innerHTML to hold the item name, but if we want to have more than one variable displayed, this gets messy fast. Don't do this, it's poor practice and the code ends up clumsy and hard to maintain.
+    // itemLi.innerHTML = "<strong>" + item.itemName + "</strong>";
+
+    // Instead we create more elements to separate things using proper markup.
+
+    // Create a span element to hold the name of the item.
+
+    let taskName = document.createElement('span');
+    taskName.setAttribute('class', 'taskname'); // We can set classes so CSS can target it nicely later.
+    taskName.innerText = item.taskName; // And we just put the text into this span, and nothing else.
+
+    let dueDate = document.createElement('span');
+    dueDate.setAttribute('class', 'dueDate'); // We can set classes so CSS can target it nicely later.
+    dueDate.innerText = item.dueDate; // And we just put the text into this span, and nothing else.
+
+    let timeInput = document.createElement('span');
+    timeInput.setAttribute('class', 'timeInput'); // We can set classes so CSS can target it nicely later.
+    timeInput.innerText = item.timeInput; // And we just put the text into this span, and nothing else.
+
+    let importanceInput = document.createElement('span');
+    importanceInput.setAttribute('class', 'importanceInput');
+    importanceInput.innerText = item.importanceInput;
+
+    // Add an element to represent the remove button
+    let itemRemove = document.createElement('button');
+    itemRemove.setAttribute('class', 'remove');
+    itemRemove.innerText = 'x'; // You can CSS this later to be pretty
+
+    // Add an event handler to the remove button. To make this work properly we need to do two things. Remove the DOM element from the document _AND_ remove the correct item from the local storage list.
+    itemRemove.addEventListener("click", function() {
+      // This allows us to remove the list li element directly which takes care of the visual removal.
+      itemLi.remove();
+
+      // And the custom removeItem function helps us to remove it from local storage.
+      removeItem(item.taskName);
+
+    });
+
+    // Add the name and remove button to the li
+    itemLi.appendChild(taskName);
+    itemLi.appendChild(timeInput);
+    itemLi.appendChild(dueDate);
+    itemLi.appendChild(importanceInput);
+
+    itemLi.appendChild(itemRemove);
+
+    // Add the li to the ul.
+    itemUl.appendChild(itemLi);
+  });
+}
+
+// Removes a specific item, by name from local storage.
+function removeItem(taskName) {
+  // Use our custom getItems() function to retrieve info from local storage. Since we need to do this in a few places, see how the custom function is more efficient?
+  let items = getItems();
+
+  // This helps us to find the array index for the item that we want to remove. It compares the information we pass in (via the itemName variable) to the information in the objects within the array. If it matches, we get a number back - i.e. items[3].
+  let itemIndex = items.findIndex(function(item) {
+    return item.taskName == taskName;
+    return item.timeInput == timeInput;
+    return item.dueDate == dueDate;
+    return item.importanceInput == importanceInput;
+  });
+
+  // We've talked about splice() in class before (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice), it removes a specific item out of the array 
+  items.splice(itemIndex, 1);
+
+  // Now we do the same process of writing information back into local storage that we did earlier.
+  items = JSON.stringify(items);
+  localStorage.setItem('items', items);
 }
